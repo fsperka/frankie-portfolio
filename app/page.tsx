b97791c743f13f0b0868da8e38f3e9ed28b1cdda
Frankie Sperka <fsperka@stanford.edu>
2026-01-07 17:38:25 -0500

    add new lines

--- app/page.tsx
+++ app/page.tsx
@@ -17,20 +17,20 @@ export default function Home() {
             Frankie Sperka
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Mechanical Engineer & Software Developer{" "}
+            Mechanical Engineer & Software Developer{" "} /n
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Work
+              Work /n
             </a>{" "}
-            or the{" "}
+            {" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               About
-            </a>{" "}
+            </a>{" "} /n
             I work at Poursteady building automated pour-over coffee machines. 
             I handle embedded systems, motion control, and hardware-software integration.
           </p>
